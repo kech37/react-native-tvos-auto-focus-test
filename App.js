52d4b1d5cc6bd178280efd112646af2663fa2ee6
kech37 <carlos.p37@gmail.com>
2021-02-11 10:25:49 +0000

    * Add secureTextEntry on password TextInput

--- App.js
+++ App.js
@@ -46,6 +46,7 @@ class App extends React.Component {
                 password: input,
               });
             }}
+            secureTextEntry={true}
             returnKeyType="done"
           />
         </View>
